2f8bd46c988996292c0ef27eeab44948b763943f
unknown <khalidabdi3101@gmail.com>
2024-09-26 05:28:18 +0300

    done Get first Number

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // Good Luck 💪🏾
+const getFirstNames = ["Ahmed","Aisha","Amina","Jaamac","Khadijah","Layla","Mohamed","Ali"]
+const groups = [["ahmed","aisha","amina"],["jamac","khadija","layla"],["mohamed","Ali"]]
+
+console.log(groups[0][0])
+console.log(groups[1][0])
+console.log(groups[2][0])
